170d124c53c374675af49fae860f13bc0c514ad9
Filip Skacanyi <filip.skacanyi@gmail.com>
2018-10-03 16:38:30 +0100

    menu and scene transition

---
+++ SlothitLeague/Assets/MenuOptionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOptionScript : MonoBehaviour {
+
+
+    public bool selected = false;
+
+    Text thisText;
+	// Use this for initialization
+	void Start () {
+       thisText = this.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+        if(selected)
+        {
+            thisText.color = Color.red;
+        }
+        else
+        {
+            thisText.color = Color.black;
+        }
+
+	}
+}
---
+++ SlothitLeague/Assets/Scripts/Menu.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    private bool selectedMenuOption;
+
+    int numberOfGoals = 5;
+
+    public KeyCode Forward;
+    public KeyCode Back;
+    public KeyCode jumpKey;
+    public KeyCode Left;
+    public KeyCode Right;
+
+    public GameObject play;
+    public GameObject numOfGoalsObject;
+
+    public GameObject scoreToWinObject; //the objetc that hold the number of goals after the scene switches
+
+    public string sceneToLoad;
+
+    // Use this for initialization
+    void Start () {
+        selectedMenuOption = true;
+        play.GetComponent<MenuOptionScript>().selected = true;
+        numOfGoalsObject.GetComponent<Text>().text = "Number of goals:" + numberOfGoals;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(Forward))
+        {
+            selectedMenuOption = true;
+
+            play.GetComponent<MenuOptionScript>().selected = true;
+            numOfGoalsObject.GetComponent<MenuOptionScript>().selected = false;
+        }
+
+
+        if (Input.GetKey(Back))
+        {
+            selectedMenuOption = false;
+
+
+            play.GetComponent<MenuOptionScript>().selected = false;
+            numOfGoalsObject.GetComponent<MenuOptionScript>().selected = true;
+        }
+
+
+        if (Input.GetKeyUp(jumpKey) && selectedMenuOption)
+        {
+            scoreToWinObject.GetComponent<ScoreToWin>().score_to_win = numberOfGoals;
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        }
+        else if(Input.GetKeyUp(jumpKey))
+        {
+            if(numberOfGoals < 10)
+            {
+                numberOfGoals++;
+
+               
+            }
+            else
+            {
+                numberOfGoals = 5;
+            }
+
+            numOfGoalsObject.GetComponent<Text>().text = "Number of goals:" + numberOfGoals;
+        }
+
+
+    }
+}
+
